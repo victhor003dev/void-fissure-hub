c70548f497b44ba1c8b746cc15f28c146f8074d6
victhor003dev <victhor003.dev@gmail.com>
2026-04-19 03:53:40 +0200

    Removed missplaced svg in button component

--- app/_components/ui/Button.tsx
+++ app/_components/ui/Button.tsx
@@ -31,16 +31,3 @@ export default function Button({
         </button>
     );
 }
-
-<svg
-    version="1.1"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 256 256"
-    fill="currentColor"
-    stroke="none"
->
-    <g transform="translate(0,256) scale(0.1,-0.1)">
-        <path d="M1256 2548 c-3 -7 -6 -75 -9 -150 l-4 -137 ..." />
-        <path d="M667 2338 c-9 -5 -17 -14 -17 -19 ..." />
-    </g>
-</svg>;
