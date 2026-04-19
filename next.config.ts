fad01968ca7fb5041d55165d7da123d5c9179a6d
victhor003dev <victhor003.dev@gmail.com>
2026-04-19 03:45:33 +0200

    Configured type-safe arguments for next-intl https://next-intl.dev/docs/workflows/typescript#messages-arguments

--- next.config.ts
+++ next.config.ts
@@ -6,5 +6,9 @@ const nextConfig: NextConfig = {
     reactCompiler: true,
 };
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin({
+    experimental: {
+        createMessagesDeclaration: "./messages/en.json",
+    },
+});
 export default withNextIntl(nextConfig);
