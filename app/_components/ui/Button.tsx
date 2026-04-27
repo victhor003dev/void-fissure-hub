dfae36d6b0bae5f592476564de718530c44b7142
victhor003dev <victhor003.dev@gmail.com>
2026-04-27 03:57:20 +0200

    Changed button sizing and added onClick

--- app/_components/ui/Button.tsx
+++ app/_components/ui/Button.tsx
@@ -1,7 +1,10 @@
+import { HTMLProps } from "react";
+
 export type ButtonProps = {
     active?: boolean;
     children?: React.ReactNode;
-    variant?: ButtonSizes;
+    size?: ButtonSizes;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
 export enum ButtonSizes {
@@ -12,20 +15,22 @@ export enum ButtonSizes {
 
 const sizeStyles: Record<ButtonSizes, string> = {
     [ButtonSizes.Normal]: "interactive-shadow-6 py-2 px-4",
-    [ButtonSizes.Square]: "interactive-shadow-4 p-2",
+    [ButtonSizes.Square]: "interactive-shadow-4 p-2 bg-red-500",
     [ButtonSizes.Big]: "interactive-shadow-4 py-2 px-8",
 };
 
 export default function Button({
     children,
-    variant = ButtonSizes.Normal,
+    size = ButtonSizes.Normal,
     active = false,
+    onClick,
 }: ButtonProps) {
-    const variantClasses = sizeStyles[variant];
+    const sizeClasses = sizeStyles[size];
 
     return (
         <button
-            className={`${variantClasses} ${active ? "active" : ""} interactive text-content  text-button font-bold uppercase`}
+            className={`${sizeClasses} ${active ? "active" : ""} interactive text-content  text-button font-bold uppercase`}
+            onClick={onClick}
         >
             {children}
         </button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import Button, { ButtonSizes } from "./_components/ui/Button";
+import ButtonGroup from "./_components/ui/ButtonGroup";
 import Icon from "./_components/ui/Icon";
 
 export default function Home() {
     return (
         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <Button>Button</Button>
-            <Button variant={ButtonSizes.Square}>
+            <Button size={ButtonSizes.Square}>
                 <Icon name="ui/LithEraRelic" size={32} />
             </Button>
-            <Button variant={ButtonSizes.Big} active={true}>
+            <Button size={ButtonSizes.Big} active={true}>
                 <Icon name="ui/VoidRelicIcon" size={64} />
             </Button>
         </div>
